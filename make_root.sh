9abe76e420b331590a09bb139c7eb0c0d9c81539
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2018-08-13 09:48:00 +0100

    Build NFS and MMC/SD kernels in one script, update .gitignore for cpio files

--- make_root.sh
+++ make_root.sh
@@ -2,12 +2,17 @@ if [ -f .config ]; then echo configured; else make ARCH=riscv defconfig; fi
 make -C ../debian-riscv64 cpio
 scripts/dtc/dtc arch/riscv/kernel/lowrisc.dts -O dtb -o arch/riscv/kernel/lowrisc.dtb
 rm -f arch/riscv/kernel/head.o
-make ARCH=riscv -j 4
+make ARCH=riscv -j 4 CONFIG_INITRAMFS_SOURCE="initramfsnfs.cpio"
 mkdir -p ../rocket-chip/riscv-tools/riscv-pk/build
 ( cd ../rocket-chip/riscv-tools/riscv-pk/build; ../configure --prefix=$RISCV --host=riscv64-unknown-elf --with-payload=../../../../riscv-linux/vmlinux --enable-logo )
 rm -f ../rocket-chip/riscv-tools/riscv-pk/build/payload.o
 make -C ../rocket-chip/riscv-tools/riscv-pk/build
-cp -p ../rocket-chip/riscv-tools/riscv-pk/build/bbl ../fpga/board/nexys4_ddr/boot.bin
-riscv64-unknown-elf-strip ../fpga/board/nexys4_ddr/boot.bin
-riscv64-unknown-elf-objdump -d -S -l vmlinux >vmlinux.dis &
-riscv64-unknown-elf-objdump -d -l -S ../rocket-chip/riscv-tools/riscv-pk/build/bbl >../rocket-chip/riscv-tools/riscv-pk/build/bbl.dis
+cp -p ../rocket-chip/riscv-tools/riscv-pk/build/bbl ../fpga/board/nexys4_ddr/boot_nfs.bin
+riscv64-unknown-elf-strip ../fpga/board/nexys4_ddr/boot_nfs.bin
+make ARCH=riscv -j 4 CONFIG_INITRAMFS_SOURCE="initramfsmmc.cpio"
+rm -f ../rocket-chip/riscv-tools/riscv-pk/build/payload.o
+make -C ../rocket-chip/riscv-tools/riscv-pk/build
+cp -p ../rocket-chip/riscv-tools/riscv-pk/build/bbl ../fpga/board/nexys4_ddr/boot_mmc.bin
+riscv64-unknown-elf-strip ../fpga/board/nexys4_ddr/boot_mmc.bin
+#riscv64-unknown-elf-objdump -d -S -l vmlinux >vmlinux.dis &
+#riscv64-unknown-elf-objdump -d -l -S ../rocket-chip/riscv-tools/riscv-pk/build/bbl >../rocket-chip/riscv-tools/riscv-pk/build/bbl.dis
