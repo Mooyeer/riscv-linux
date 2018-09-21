573cc7f8050f9265aa009628a6140fe1a911cfb5
Dr Jonathan Kimmitt <jrrk2@cam.ac.uk>
2018-09-21 06:33:55 +0100

    Merged patches from linux-4.18.0

---
+++ arch/riscv/kernel/time.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2012 Regents of the University of California
+ * Copyright (C) 2017 SiFive
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation, version 2.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+#include <linux/clocksource.h>
+#include <linux/delay.h>
+#include <asm/sbi.h>
+
+unsigned long riscv_timebase;
+
+void __init time_init(void)
+{
+	timer_probe();
+	lpj_fine = riscv_timebase / HZ;
+}
