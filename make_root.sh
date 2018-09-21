d2df5c38f0b872f0e0dd37066f280190a2de128c
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2018-09-21 12:34:01 +0100

    Move DTB to riscv-pk, update cpio parameters in build script

--- make_root.sh
+++ make_root.sh
@@ -1,15 +1,14 @@
 if [ -f .config ]; then echo configured; else make ARCH=riscv defconfig; fi
 make -C ../debian-riscv64 cpio
-scripts/dtc/dtc arch/riscv/kernel/lowrisc.dts -O dtb -o arch/riscv/kernel/lowrisc.dtb
-rm -f arch/riscv/kernel/head.o
-make ARCH=riscv -j 4 CONFIG_INITRAMFS_SOURCE="initramfsnfs.cpio"
+make ARCH=riscv -j 4 CROSS_COMPILE=riscv64-unknown-linux-gnu- CONFIG_INITRAMFS_SOURCE="initramfsnfs.cpio"
 mkdir -p ../rocket-chip/riscv-tools/riscv-pk/build
 ( cd ../rocket-chip/riscv-tools/riscv-pk/build; ../configure --prefix=$RISCV --host=riscv64-unknown-elf --with-payload=../../../../riscv-linux/vmlinux --enable-logo )
+scripts/dtc/dtc ../rocket-chip/riscv-tools/riscv-pk/machine/lowrisc.dts -O dtb -o ../rocket-chip/riscv-tools/riscv-pk/build/lowrisc.dtb
 rm -f ../rocket-chip/riscv-tools/riscv-pk/build/payload.o
 make -C ../rocket-chip/riscv-tools/riscv-pk/build
 cp -p ../rocket-chip/riscv-tools/riscv-pk/build/bbl ../fpga/board/nexys4_ddr/boot_nfs.bin
 riscv64-unknown-elf-strip ../fpga/board/nexys4_ddr/boot_nfs.bin
-make ARCH=riscv -j 4 CONFIG_INITRAMFS_SOURCE="initramfsmmc.cpio"
+make ARCH=riscv -j 4 CROSS_COMPILE=riscv64-unknown-linux-gnu- CONFIG_INITRAMFS_SOURCE="initramfsmmc.cpio"
 rm -f ../rocket-chip/riscv-tools/riscv-pk/build/payload.o
 make -C ../rocket-chip/riscv-tools/riscv-pk/build
 cp -p ../rocket-chip/riscv-tools/riscv-pk/build/bbl ../fpga/board/nexys4_ddr/boot_mmc.bin
